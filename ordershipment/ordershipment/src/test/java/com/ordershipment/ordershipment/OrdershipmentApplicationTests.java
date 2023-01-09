2bce9e468650884b3dc5ec295a6fc625cc96626b
venkatarepala <121960106+venkatarepala@users.noreply.github.com>
2023-01-09 20:34:48 +0530

    Created jar file and uploaded the directory
    Provided Jar file in target folder

---
+++ ordershipment/ordershipment/src/test/java/com/ordershipment/ordershipment/OrdershipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordershipment.ordershipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdershipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
